2042c8e2b40263aab7587f877596bfb03f6658dd
Miguel <mmaso@cimne.upc.edu>
2019-07-08 17:33:41 +0200

    making test exmple work

--- applications/convection_diffusion_application/test_examples/bfecc_test/test_bfecc.py
+++ applications/convection_diffusion_application/test_examples/bfecc_test/test_bfecc.py
@@ -3,24 +3,24 @@ import ProjectParameters
 domain_size = 2
 #
 #
-import sys
-sys.path.append(ProjectParameters.kratos_path)
-from KratosMultiphysics import *
-from KratosMultiphysics.FluidDynamicsApplication import * #need this for the cube mesher
-from KratosMultiphysics.MeshingApplication import *
-from KratosMultiphysics.ConvectionDiffusionApplication import *
+# import sys
+# sys.path.append(ProjectParameters.kratos_path)
+import KratosMultiphysics as KM
+import KratosMultiphysics.ConvectionDiffusionApplication as CD
+# from KratosMultiphysics.FluidDynamicsApplication import * #need this for the cube mesher
+# from KratosMultiphysics.MeshingApplication import *
 
 
 # defining a model part for the fluid
-base_model_part = ModelPart("BasePart")
-base_model_part.AddNodalSolutionStepVariable(VELOCITY) ##needed to compute the normal
-base_model_part.AddNodalSolutionStepVariable(TEMPERATURE) ##needed to compute the normal
+model = KM.Model()
+base_model_part = model.CreateModelPart("BasePart")
+base_model_part.AddNodalSolutionStepVariable(KM.VELOCITY) ##needed to compute the normal
+base_model_part.AddNodalSolutionStepVariable(KM.TEMPERATURE) ##needed to compute the normal
 
 
 #reading the base model part
-model_part_io_fluid = ModelPartIO("base")
+model_part_io_fluid = KM.ModelPartIO("base")
 model_part_io_fluid.ReadModelPart(base_model_part)
-
 
 
 # setting up the buffer size: SHOULD BE DONE AFTER READING!!!
@@ -54,12 +54,12 @@ for node in base_model_part.Nodes:
     ylocal = node.Y - 0.5
     r = math.sqrt( (xlocal)**2 + (ylocal)**2 )
     if( r < 0.45):
-        node.SetSolutionStepValue(VELOCITY_X,0,-ylocal)
-        node.SetSolutionStepValue(VELOCITY_Y,0,xlocal)
+        node.SetSolutionStepValue(KM.VELOCITY_X, 0, -ylocal)
+        node.SetSolutionStepValue(KM.VELOCITY_Y, 0, xlocal)
     
         d = math.sqrt( (xlocal - xc)**2 + (ylocal-yc)**2 ) - 0.1
         
-        node.SetSolutionStepValue(TEMPERATURE,0,d)
+        node.SetSolutionStepValue(KM.TEMPERATURE, 0, d)
         #if(d <= 0.0):
             #node.SetSolutionStepValue(TEMPERATURE,0,-1.0)
 
@@ -80,12 +80,12 @@ gid_io_base.initialize_results(base_model_part)
 
 
 #mount the search structure
-locator = BinBasedFastPointLocator2D(base_model_part)
+locator = KM.BinBasedFastPointLocator2D(base_model_part)
 locator.UpdateSearchDatabase()
 #locator.UpdateSearchDatabaseAssignedSize(0.01)
 
 #construct the utility to move the points
-bfecc_utility = BFECCConvection2D(locator)
+bfecc_utility = CD.BFECCConvection2D(locator)
 
 base_model_part.CloneTimeStep(0.00)
 
@@ -99,14 +99,13 @@ while( t < final_time):
     base_model_part.CloneTimeStep(t)
     
     print("time = ",t)
-    bfecc_utility.BFECCconvect(base_model_part,TEMPERATURE,VELOCITY,substepping)
-        
+    bfecc_utility.BFECCconvect(base_model_part, KM.TEMPERATURE, KM.VELOCITY, substepping)
+
     gid_io_base.write_results(
             t,
             base_model_part,
             ProjectParameters.nodal_results,
             ProjectParameters.gauss_points_results)
-
 
     step +=1
 
